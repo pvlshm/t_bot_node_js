d1022f482eeac26527b46c2cad7e0db058f22a66
PaulShum <pvlshm@gmail.com>
2017-04-05 13:01:24 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-    var TelegramBot = require('node-telegram-bot-api');
 
-    // Устанавливаем токен, который выдавал нам бот.
-    var token = '339457527:AAHZUxnbZ7sfSHlbVtNVQ1RD_KY9gwCZGqU';
-    // Включить опрос сервера
-    var bot = new TelegramBot(token, [polling: true]);
+var TelegramBot = require('node-telegram-bot-api'),
+    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+    telegram = new TelegramBot("339457527:AAHZUxnbZ7sfSHlbVtNVQ1RD_KY9gwCZGqU", { polling: true });
 
-    // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
-    bot.onText(/\/echo (.+)/, function (msg, match) {
-      var fromId = msg.from.id;
-      var resp = match[1];
-      bot.sendMessage(fromId, resp);
-    });
-
-    // Простая команда без параметров.
-    bot.on('message', function (msg) {
-      var chatId = msg.chat.id;
-      // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-      var photo = 'cats.png';
-      bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
-    });
+telegram.on("text", (message) => {
+  telegram.sendMessage(message.chat.id, "Hello world");
+});
